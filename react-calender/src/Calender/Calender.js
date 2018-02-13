3c6db68e7995c75c61040180004ea0a49773b047
ravshrestha9 <shrestharavee1@gmail.com>
2018-02-13 01:19:35 -0600

    Create Demo Calender

--- react-calender/src/Calender/Calender.js
+++ react-calender/src/Calender/Calender.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react'
 import BigCalendar from 'react-big-calendar'
-import {connect} from 'react-redux'
 import moment from 'moment'
+import './Calender.css'
 
 BigCalendar.momentLocalizer(moment);
 
+
+
 const Calender = props => (
-<div id = "calender">
+<div id= "calender">
     <BigCalendar
     events ={[]}
+    style={{height: 800}}
     startAccessor = 'startDate'
     endAccessor = 'endDate'
     />
--- react-calender/src/index.js
+++ react-calender/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+require("react-big-calendar/lib/css/react-big-calendar.css")
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
