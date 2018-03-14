c5121870cb212371d7b256a408de175053996454
Pratik Siwakoti <Pratik Siwakoti>
2018-03-13 20:57:20 -0500

    Connecting to the database and querying stuff from php

--- react-calender/src/components/containers/Forms/LoginForm/login.php
+++ react-calender/src/components/containers/Forms/LoginForm/login.php
@@ -1,9 +1,38 @@
 <?php
-$db = new mysqli('localhost','root','','anursing');
 
-$result = $db->query("SELECT fname FROM user")
+function OpenCon()
+ {
+ $host = "localhost";
+ $user = "root";
+ $password = "";
+ $database = "anursing";
 
-print_r($result);
 
-echo $_POST['username'];
-echo $_POST['password'];
+ $conn = new mysqli($host, $user, $password,$database) or die("Connect failed: %s\n". $conn -> error);
+
+
+ return $conn;
+ }
+
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+
+ $conn = OpenCon();
+
+echo "Connected Successfully"."<br />";
+$query = "SELECT cwid, fname, lname from user";
+
+$result = $conn->query($query);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "cwid: " . $row["cwid"]. " - Name: " . $row["fname"]. " " . $row["lname"]. "<br />";
+    }
+} else {
+    echo "0 results";
+}
+CloseCon($conn);
+
+?>
